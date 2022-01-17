c6c66265ff536aa34f28d30e0bf8a578c762f017
gemece <30463151+gemece@users.noreply.github.com>
2022-01-17 01:58:30 +0100

    Cors configuration

--- spring-boot-backend-apirest-movie/src/main/java/com/springboot/backend/apirest/movie/auth/ResourceServerConfig.java
+++ spring-boot-backend-apirest-movie/src/main/java/com/springboot/backend/apirest/movie/auth/ResourceServerConfig.java
@@ -22,7 +22,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api").permitAll()
-		.anyRequest().authenticated();
+		.anyRequest().authenticated()
+		.and().cors().configurationSource(corsConfigurationSource());
 	}
 	
 	@Bean
